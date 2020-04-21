f8ab8037abb032784e2c8be9ba59880c160bc184
Paul Asu <pasu@mail.sfsu.edu>
2020-04-20 21:47:16 -0700

    Updates my projects with new knowledge

--- application/routes/dbtest.js
+++ application/routes/dbtest.js
@@ -2,15 +2,12 @@ const express = require('express');
 const router = express.Router();
 const db     = require('../conf/database');
 
-
-
 router.get('/getAllUsers', (req, res, next) =>{
     db.query('SELECT * from users;',(err, results, fields) => 
     {
         console.log(results);
         res.send(results);
     })
-
 });
 
 router.get('/getAllPosts', (req, res, next) =>{
@@ -19,7 +16,6 @@ router.get('/getAllPosts', (req, res, next) =>{
         console.log(results);
         res.send(results);
     })
-
 });
 
 // trying with promise
